4657690ce7bc1d381c47b4f97dfab64538419c09
Fredy Ramirez <fredy.ramirez@honeywell.com>
2019-06-18 03:09:48 -0500

    Add sentiment processing
    Signed-off-by: Fredy Ramirez <fredy.ramirez@honeywell.com>

---
+++ PersonnelServices/Controllers/SentimentController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PersonnelServices.DAL.Interface;
+
+namespace PersonnelServices.Controllers
+{
+    public class SentimentController : PersonnelBaseController
+    {
+        public SentimentController(IRepository repository, IConfiguration configuration)
+            : base(repository, configuration)
+        {
+
+        }
+
+        [HttpGet]
+        [Route("sentiment/{text}/language/{language}")]
+        public async Task<IActionResult> PostSentiment(string text, string language)
+        {
+            try
+            {
+                if (text.Length > 0)
+                {
+                    if (language.Length == 0)
+                    {
+                        language = "es";
+                    }
+
+                    SentimentProcessing sentiment = new SentimentProcessing(Configuration);
+
+                    double result = await sentiment.SentimentRequest(text, language).ConfigureAwait(false);
+
+                    if (result > 0)
+                    {
+
+
+                        return Ok(result);
+                    }
+                    else
+                    {
+                        return NoContent();
+                    }
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500, $"Server Internal Error {ex}");
+            }
+        }
+    }
+}
---
+++ PersonnelServices/Controllers/SentimentProcessing.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PersonnelServices.Controllers
+{
+    public class SentimentProcessing
+    {
+        private readonly string subscriptionKey;
+        private readonly string uriBase;
+        private IConfiguration Configuration;
+
+        public SentimentProcessing(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            subscriptionKey = Configuration.GetSection("SentimentConnectionStrings").GetValue<string>("subscriptionKey");
+            uriBase = Configuration.GetSection("SentimentConnectionStrings").GetValue<string>("uriBase");
+        }
+
+        public Task<double> SentimentRequest(string text, string lang)
+        {
+            var client = new WebClient();
+            client.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            client.Headers.Add("Content-Type", "application/json");
+            client.Headers.Add("Accept", "application/json");
+
+            // Determine sentiment
+            var postData = @"{""documents"":[{""id"":""1"", ""language"":""@language"", ""text"":""@sampleText""}]}".Replace("@sampleText", text).Replace("@language", lang);
+            var response = client.UploadString(uriBase, postData);
+
+            var sentimentStr = new Regex(@"""score"":([\d.]+)").Match(response).Groups[1].Value;
+
+            return  Task.FromResult(Convert.ToDouble(sentimentStr, System.Globalization.CultureInfo.InvariantCulture));
+        }
+    }
+}
