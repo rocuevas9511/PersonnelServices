25752eb9bd547aab4c09375d23d2dc0eb849b7aa
Fredy Ramirez <fredy.ramirez@honeywell.com>
2019-06-17 20:12:12 -0500

    New initial commit

---
+++ PersonnelServices/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace PersonnelServices
+{
+    public class Program
+    {
+        protected Program()
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Run();
+        }
+
+        public static IWebHost CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
